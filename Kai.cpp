5a62d40c138b646e6d93f50444674797f1b79c50
kaiser-114 <areezilla@gmail.com>
2026-03-12 14:52:55 +0700

    add implement insertion sort system

--- Kai.cpp
+++ Kai.cpp
@@ -27,3 +27,25 @@ void input() {  // procedure input
         cin >> arr[i];
     }
 }
+
+void insertionSort() { // procedure insertion sort
+    int temp;
+    int j;
+
+    for (int i = 1; i < n; i++) {
+        temp = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+
+        cout << "\nStep " << i << ": ";
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";
+        }
+    }
+}

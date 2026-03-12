ade722ce6fab6f3558f21a7e7d0f44741d82f056
kaiser-114 <areezilla@gmail.com>
2026-03-12 14:51:18 +0700

    add implement void input

--- Kai.cpp
+++ Kai.cpp
@@ -3,3 +3,16 @@ using namespace std;
 
 int arr[20];    // membuat array dengan panjang data 20
 int n;          // membuat variabel inputan n
+
+void input() {  // procedure input
+    while (true) {
+        cout << "Masukan jumlah data Pada Array : ";
+        cin >> n;
+
+             if (n <= 20) {
+            break;
+        }
+        else {
+            cout << "\nArray tidak boleh lebih dari 20\n";
+        }
+    }
